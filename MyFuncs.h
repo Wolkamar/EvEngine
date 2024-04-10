33299b14def59630366fbde174fb99fd09ec01f4
Wolkamar <Wolkamar@gmail.com>
2024-04-11 01:42:49 +0700

    lecture 1

---
+++ MyFuncs.h
@@ -0,0 +1,16 @@
+#include <string>
+
+int getNum()
+{
+	return 42;
+}
+
+std::string str(int x)
+{
+	return std::to_string(x);
+}
+
+std::string str(double x)
+{
+	return std::to_string(x);
+}
---
+++ x64/Debug/start.bat
@@ -0,0 +1,2 @@
+EvEngine.exe
+ECHO
